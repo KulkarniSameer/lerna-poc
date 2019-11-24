52c037a3351c84cfbabb7bd9f90ba63f4b832bff
Sameer Kulkarni <sameer.sanjayk@gmail.com>
2019-11-24 23:11:01 +0530

    added support to greet good morning

--- packages/package1/index.js
+++ packages/package1/index.js
@@ -2,6 +2,11 @@ function sayHello() {
   console.log("Hello from package 1");
 }
 
+function greetGoodMorning() {
+  console.log("Hey! Good Morning!");
+}
+
 module.exports = {
-  sayHello
+  sayHello,
+  greetGoodMorning
 };
