828e79aa0fc7cc13fa7d870a8e0dd2f3263be1ce
Sameer Kulkarni <sameer.sanjayk@gmail.com>
2019-11-24 23:15:11 +0530

    added log in @lerna-poc/core

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,6 +1,8 @@
 const package1 = require("@lerna-poc/package1");
 const package2 = require("@lerna-poc/package2");
 
+console.log("Running from @lerna-poc/core");
+
 package1.sayHello();
 package1.greetGoodMorning();
 package2.sayHello();
